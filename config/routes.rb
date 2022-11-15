511a750d97a5656077b2acbbe2f91139eb38de42
Percy <edoumps@gmail.com>
2022-11-15 08:26:38 +0100

    Configured custom routes for pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "portfolios#index"
+  root "portfolios#home"
 end
